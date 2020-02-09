477f8154746abaee31dc7ac5c6c7f4c7c4672b41
Ian Hawkins <ian@hawkapp.com>
2020-02-09 15:59:52 -0800

    Move input checkers into their own file

---
+++ error-checking.js
@@ -0,0 +1,43 @@
+/*globals COLUMN_WORKSHOP_NAME, COLUMN_WORKSHOP_CAPACITY, COLUMN_FIRST_NAME,
+ * COLUMN_LAST_NAME, COLUMN_GRADE, PREFERENCE_COLUMNS, Logger */
+// Functions that check the inputs for valid types can be extended to check for other params
+
+function workshop_input_checker(name, capacity, location, i) {
+    if (typeof name !== "string") {
+        throw new Error(
+            "Invalid input at row " + i + " column " + COLUMN_WORKSHOP_NAME
+        );
+    }
+    if (typeof capacity !== "number") {
+        throw new Error(
+            "Invalid input at row " + i + " column " + COLUMN_WORKSHOP_CAPACITY
+        );
+    }
+}
+
+function student_input_checker(firstName, lastName, grade, workshopNum, j) {
+    if (typeof firstName !== "string") {
+        throw new Error(
+            "Invalid input at row " + j + " column " + COLUMN_FIRST_NAME
+        );
+    }
+    if (typeof lastName !== "string") {
+        throw new Error(
+            "Invalid input at row " + j + " column " + COLUMN_LAST_NAME
+        );
+    }
+    if (typeof grade !== "string") {
+        throw new Error(
+            "Invalid input at row " + j + " column " + COLUMN_GRADE
+        );
+    }
+}
+
+function preference_input_checker(workshopNum, j, k) {
+    Logger.log("Checking preferred workshop with type " + typeof workshopNum);
+    if (typeof workshopNum !== "number") {
+        throw new Error(
+            "Invalid input at row " + j + " column " + PREFERENCE_COLUMNS[k]
+        );
+    }
+}
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-/*globals SpreadsheetApp, Logger, Matcher */
+/*globals SpreadsheetApp, Logger, Matcher, workshop_input_checker,
+ * student_input_checker, preference_input_checker */
 
 var COLUMN_FIRST_NAME = 10;
 var COLUMN_LAST_NAME = 11;
@@ -68,46 +69,6 @@ function onOpen() {
         .createMenu("Great Explorations")
         .addItem("Match Girls to Workshops", "matchGirls")
         .addToUi();
-}
-
-///// Functions that check the inputs for valid types can be extended to check for other params//////
-function workshop_input_checker(name, capacity, location, i) {
-    if (typeof name !== "string") {
-        throw new Error(
-            "Invalid input at row " + i + " column " + COLUMN_WORKSHOP_NAME
-        );
-    }
-    if (typeof capacity !== "number") {
-        throw new Error(
-            "Invalid input at row " + i + " column " + COLUMN_WORKSHOP_CAPACITY
-        );
-    }
-}
-
-function student_input_checker(firstName, lastName, grade, workshopNum, j) {
-    if (typeof firstName !== "string") {
-        throw new Error(
-            "Invalid input at row " + j + " column " + COLUMN_FIRST_NAME
-        );
-    }
-    if (typeof lastName !== "string") {
-        throw new Error(
-            "Invalid input at row " + j + " column " + COLUMN_LAST_NAME
-        );
-    }
-    if (typeof grade !== "string") {
-        throw new Error(
-            "Invalid input at row " + j + " column " + COLUMN_GRADE
-        );
-    }
-}
-
-function preference_input_checker(workshopNum, j, k) {
-    if (typeof workshopNum !== "number") {
-        throw new Error(
-            "Invalid input at row " + j + " column " + PREFERENCE_COLUMNS[k]
-        );
-    }
 }
 
 /**
