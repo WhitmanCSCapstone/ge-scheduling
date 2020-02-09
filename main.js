829fe284a5bdc7f433ad4f429d426a9d7d07f6ca
Trung Vu <vuta@noyce.whitman.edu>
2020-02-09 15:11:25 -0800

    Its all good in the hood

--- main.js
+++ main.js
@@ -71,34 +71,43 @@ function onOpen() {
 }
 
 ///// Functions that check the inputs for valid types can be extended to check for other params//////
-function workshop_input_checker(name, capacity, location, i){
-  
-        if (typeof(name) !== "string"){
-            throw "Invalid input at row " + i + " column " + COLUMN_WORKSHOP_NAME;      
-        }
-        if (typeof(capacity) !== "number"){
-            throw "Invalid input at row " + i + " column " + COLUMN_WORKSHOP_CAPACITY;
-        }
-}
-  
-  
-function student_input_checker(firstName, lastName, grade, workshopNum, j){
-  
-        if (typeof(firstName) !== "string"){
-            throw "Invalid input at row " + j + " column " + COLUMN_FIRST_NAME;      
-        }
-        if (typeof(lastName) !== "string"){
-            throw "Invalid input at row " + j + " column " + COLUMN_LAST_NAME;
-        }
-        if (typeof(grade) !== "string"){
-            throw "Invalid input at row " + j + " column " + COLUMN_GRADE;
-        }
+function workshop_input_checker(name, capacity, location, i) {
+    if (typeof name !== "string") {
+        throw new Error(
+            "Invalid input at row " + i + " column " + COLUMN_WORKSHOP_NAME
+        );
+    }
+    if (typeof capacity !== "number") {
+        throw new Error(
+            "Invalid input at row " + i + " column " + COLUMN_WORKSHOP_CAPACITY
+        );
+    }
 }
 
-function preference_input_checker(workshopNum, j){
-          if (typeof(workshopNum) !== "number"){
-            throw "Invalid input at row " + j + " column " + PREFERENCE_COLUMNS[k];
-        }
+function student_input_checker(firstName, lastName, grade, workshopNum, j) {
+    if (typeof firstName !== "string") {
+        throw new Error(
+            "Invalid input at row " + j + " column " + COLUMN_FIRST_NAME
+        );
+    }
+    if (typeof lastName !== "string") {
+        throw new Error(
+            "Invalid input at row " + j + " column " + COLUMN_LAST_NAME
+        );
+    }
+    if (typeof grade !== "string") {
+        throw new Error(
+            "Invalid input at row " + j + " column " + COLUMN_GRADE
+        );
+    }
+}
+
+function preference_input_checker(workshopNum, j, k) {
+    if (typeof workshopNum !== "number") {
+        throw new Error(
+            "Invalid input at row " + j + " column " + PREFERENCE_COLUMNS[k]
+        );
+    }
 }
 
 /**
@@ -112,14 +121,12 @@ function main() {
         var name = workshopData[i][COLUMN_WORKSHOP_NAME];
         var number = i;
         var capacity = workshopData[i][COLUMN_WORKSHOP_CAPACITY];
-        var location = workshopData[i][COLUMN_WORKSHOP_BUILDING] + " " + workshopData[i][COLUMN_WORKSHOP_ROOM];
-        
-        workshop_input_checker(name,capacity, location, i);
-        
+        var location =
+            workshopData[i][COLUMN_WORKSHOP_BUILDING] +
+            " " +
+            workshopData[i][COLUMN_WORKSHOP_ROOM];
 
-
-        
-        
+        workshop_input_checker(name, capacity, location, i);
 
         matcher.addNewWorkshop(name, number, capacity, location);
     }
@@ -142,12 +149,8 @@ function main() {
                     preferredWorkshop.indexOf(")")
                 )
             );
-         preference_input_checker(workshopNum, j);
-          
-          
-          
-          
-          
+            preference_input_checker(workshopNum, j, k);
+
             if (preferenceNums.indexOf(workshopNum) === -1) {
                 preferenceNums.push(workshopNum);
             }
