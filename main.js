186af2f69d2768f144d9cbbc80b3bc7338c01d9e
Chris-Pyles <cpyles4@gmail.com>
2020-03-12 18:17:35 -0700

    rebasing to master (some code looks duplicated)

--- main.js
+++ main.js
@@ -199,13 +199,4 @@ function populateSheet(outputSheet, matcher) {
     outputSheet
         .getRange(outputSheet.getLastRow() + 1, 1, rowCount, columnCount)
         .setValues(studentLines);
-
-    let colors = ["PaleGreen", "PaleTurquoise", "Pink", "PeachPuff", "White"];
-    for (let i = 0; i < 5; i++) {
-        let colorColumn = (i*3) + 1;
-        let color = colors[i];
-        outputSheet
-        .getRange(1, colorColumn, outputSheet.getLastRow(), colorColumn + 2)
-        .setBackground(color);
-    }
 }
